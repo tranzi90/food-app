1aeb423ae9551367209eccdab1d2ce23b8b41341
denis_levchenko <tranzi90@gmail.com>
2022-12-11 22:09:24 +0300

    write nav styles

--- src/app/components/ui/layout/header/header.module.ts
+++ src/app/components/ui/layout/header/header.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { HeaderComponent } from './header.component'
 import { MatIconModule } from '@angular/material/icon'
-import { RouterLink } from '@angular/router'
+import { RouterLink, RouterLinkActive } from '@angular/router'
 
 @NgModule({
     declarations: [HeaderComponent],
-    imports: [CommonModule, MatIconModule, RouterLink],
+    imports: [CommonModule, MatIconModule, RouterLink, RouterLinkActive],
     exports: [HeaderComponent],
 })
 export class HeaderModule {}
