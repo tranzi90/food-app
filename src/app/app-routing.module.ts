3c641261e7c29261c2a4d6f5b7fc5afc17d6aca8
denis_levchenko <tranzi90@gmail.com>
2022-12-12 22:06:06 +0300

    add nav links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './components/screens/home/home.component'
+import { FavoritesComponent } from './components/screens/favorites/favorites.component'
+import { ProfileComponent } from './components/screens/profile/profile.component'
+import { SaleComponent } from './components/screens/sale/sale.component'
 
-const routes: Routes = []
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'favorites',
+        component: FavoritesComponent,
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+    },
+    {
+        path: 'sale',
+        component: SaleComponent,
+    },
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/screens/sale/sale.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SaleComponent } from './sale.component';
+
+
+
+@NgModule({
+  declarations: [
+    SaleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SaleModule { }
