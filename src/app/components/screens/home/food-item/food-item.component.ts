9bd9cc173e64419c15afd04c78e37eeb689fc0c8
denis_levchenko <tranzi90@gmail.com>
2022-12-15 22:21:28 +0300

    style food product card, create search field

--- src/app/components/screens/home/food-item/food-item.component.ts
+++ src/app/components/screens/home/food-item/food-item.component.ts
@@ -4,7 +4,7 @@ import { IFood } from '../../../../services/food/food.interface'
 @Component({
     selector: 'app-food-item',
     templateUrl: './food-item.component.html',
-    styleUrls: ['./food-item.component.css'],
+    styleUrls: ['./food-item.component.scss'],
 })
 export class FoodItemComponent {
     @Input() food: IFood
--- src/app/components/screens/home/home.module.ts
+++ src/app/components/screens/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common'
 import { HomeComponent } from './home.component'
 import { FoodItemComponent } from './food-item/food-item.component'
 import { RouterLink } from '@angular/router'
+import { SearchComponent } from './search/search.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-    declarations: [HomeComponent, FoodItemComponent],
-    imports: [CommonModule, RouterLink],
+    declarations: [HomeComponent, FoodItemComponent, SearchComponent],
+    imports: [CommonModule, RouterLink, FormsModule],
 })
 export class HomeModule {}
